422660728444cf655f26b9523515ec2d5a26196a
rizk <rizkdotai@gmail.com>
2023-09-10 21:17:43 +0300

    Add dynamic grid

--- code.js
+++ code.js
@@ -1,27 +1,52 @@
 
-const sizeOfGrid = 16
+
 const container = document.querySelector('.container')
-
-
+const colorPrompt = 'red'
+const resizeBtn = document.querySelector('button')
+      
 const createGrid = (amtOfGrids) => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('wrapper')
   for (let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div')
     row.classList.add('grid-row')
     
- 
-    for (let j = 0; j < amtOfGrids; j++ ) {
+    for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960 / amtOfGrids
       const gridBox = document.createElement('div')
       gridBox.classList.add('grid-box')
-      row.appendChild(gridBox)
-        // change background color 
-      gridBox.addEventListener('mouseenter', () => {
-        gridBox.style.backgroundColor = 'black'})
+      gridBox.style.width = `${widthAndHeight}px`
+      gridBox.style.height = `${widthAndHeight}px`
       
-    } 
-    container.appendChild(row)
+      row.appendChild(gridBox)
+      gridBox.addEventListener('mouseenter', () => {
+gridBox.style.backgroundColor = 'black'})
+      
+    }
+  
+ wrapper.appendChild(row) 
   }
+  container.appendChild(wrapper)
 }
-//const resetButton = document.querySelector('button')
 
-createGrid(sizeOfGrid)
+
+
+resizeBtn.addEventListener('click', () => {
+  let userSize = Number(prompt('What dimensions do you want for the grid?'))
+  while (userSize > 100) {
+    userSize = Number(prompt('Pick a number less than or equal to 100'))
+  }
+  const wrapper = document.querySelector('.wrapper')
+  
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+  wrapper.remove()
+  createGrid(userSize)
+  }
+  
+})
+
+
+createGrid(16)
 
